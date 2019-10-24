0c199cbd5a823c8ffbda6778683bab10b0cd91f9
amenpunk <ondasycircuitos@gmail.com>
2019-10-24 12:52:17 -0600

    post var in body

--- body.php
+++ body.php
@@ -11,8 +11,23 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo );
   <head>
     <body>
       <h1>
-          HOLA MUNDO DESDE EL BODY
+        HOLA MUNDO DESDE EL BODY
       </h1>
+    <?php
+
+    $id = $_GET['id'];
+    echo var_dump($id);
+
+    $sql = 'SELECT primer_nombre,primer_apellido FROM paciente where id_paciente='.$id;
+    $stmt = sqlsrv_query( $conn, $sql );
+      if( $stmt === false) {
+        die( print_r( sqlsrv_errors(), true) );
+      }
+    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+        echo $row['primer_nombre'].", ".$row['primer_apellido']."<br />";
+    }
+    sqlsrv_free_stmt( $stmt); ?>
+
     </body>
   </head>
 </html>
